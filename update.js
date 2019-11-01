26efc31112b628a59a28d60c30a1d9797a0e701a
catatanlamers <55016668+catatanlamers@users.noreply.github.com>
2019-11-01 08:31:28 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,3 +1,11 @@
-var uphik = "Uphik Lamers";
-console.log(uphik);
-alert(uphik);
+<li>
+                        <a href="" id="probutton" class="item-link close-panel item-content ">
+                            <div class="item-media">
+                                <i class="fa fa-shopping-cart"></i>
+                            </div>
+                            <div class="item-inner">
+                                <div class="item-title">PRO (No ADS!)</div>
+							</div>
+                            
+                        </a>
+                    </li>
