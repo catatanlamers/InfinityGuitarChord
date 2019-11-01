a30b89b1d4784776d675f9161b3ab7cd1c55a45d
catatanlamers <55016668+catatanlamers@users.noreply.github.com>
2019-11-01 08:39:32 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,3 +1,3 @@
 var inidia ='<li><a href="" id="probutton" class="item-link close-panel item-content "><div class="item-media"><i class="fa fa-shopping-cart"></i></div><div class="item-inner"><div class="item-title">PRO (No ADS!)</div></div></a></li>';
 
-$$('#isi_li').append(inidia);
+$('#isi_li').append(inidia);
